9e494057156eeedd5e71a33d6831381318ffaedb
dauyezkhan <dauyezkhan@openwaygroup.com>
2026-01-15 11:01:32 +0500

    added var with buildDiscarder oprion

---
+++ vars/discardOldBuilds.groovy
@@ -0,0 +1,14 @@
+def call(){
+  pipeline {
+    options {
+      buildDiscarder(logRotator(daysToKeepStr: '365', numToKeepStr: '30')
+    }
+    agent { label "ansible" }
+    stages {
+      stage("Example")
+        steps{
+          sh 'echo Hello World :)'
+      }
+    }
+  }
+}
