40b2310b9b401af02d056adfcf1fc2d7e0cce212
bl1nkker <nurovich14@gmail.com>
2026-02-09 17:27:59 +0500

    added pipeline

--- vars/dbHouseKeeping.groovy
+++ vars/dbHouseKeeping.groovy
@@ -1,12 +1,20 @@
 def call(){
-  def pyScript = libraryResource("clear_table.py")
-  withCredentials([[$class: 'UsernamePasswordMultiBinding',
-        credentialsId: 'PSQL',
-        usernameVariable: 'PSQL_MQ_USER',
-        passwordVariable: 'PSQL_MQ_PASSWORD'
-      ]]) {
-    echo "about to run pyScript"
-    status = sh(script: pyScript, returnStatus: true)
-    echo "run pyScript done"
+  pipeline {
+    agent { label 'master' }
+    stages {
+      stage('Run pyscript'){
+        def pyScript = libraryResource("clear_table.py")
+        withCredentials([[$class: 'UsernamePasswordMultiBinding',
+            credentialsId: 'PSQL',
+            usernameVariable: 'PSQL_MQ_USER',
+            passwordVariable: 'PSQL_MQ_PASSWORD'
+        ]]) {
+          echo "about to run pyScript"
+          status = sh(script: pyScript, returnStatus: true)
+          echo "run pyScript done"
+        }
+      }
+    }
   }
 }
+
