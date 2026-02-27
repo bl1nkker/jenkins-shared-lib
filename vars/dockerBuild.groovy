002a8b61ab21b339ac4e48d56ac9bd2962817013
bl1nkker <nurovich14@gmail.com>
2026-02-27 14:45:46 +0500

    added completed dockerBuild pipeline

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -32,42 +32,124 @@ def call(){
                 steps{
                     // docker login
                     echo "==> Using credentials ID: DOCKER"
-                    withCredentials([usernamePassword(
-                                        credentialsId:    'DOCKER',
-                                        usernameVariable: 'DOCKER_REGISTRY_USER',
-                                        passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]){
-                        sh ('echo ${DOCKER_REGISTRY_PASSWORD} | docker login -u ${DOCKER_REGISTRY_USER} --password-stdin ')
+                    withCredentials([
+                        usernamePassword(
+                            credentialsId: "DOCKER",
+                            usernameVariable: "DOCKER_REGISTRY_USER",
+                            passwordVariable: "DOCKER_REGISTRY_PASSWORD"
+                        )
+                    ]){
+                        sh ("echo ${DOCKER_REGISTRY_PASSWORD} | docker login -u ${DOCKER_REGISTRY_USER} --password-stdin")
                     }
                 }
             }
-            stage("Docker build"){
-                steps{
-                    echo "docker build here..."
-                    // Retrieve BASE_TAG
-                    // if branch == "master": BASE_TAG = master
-                    // if branch == "staging": BASE_TAG = staging
-                    // if branch == "feature": BASE_TAG = sh("git describe --tags") # 1.0.1-4-gfa58fb7
-                    
-                    // Docker build (compose or dockerfile) with BASE_TAG
-                    // must retrieve docker images as output
-
-                    // Retag builded images:
-                    // if branch == "master":
-                    //      new_tag = sh("git tag") + 1 # 1.0.2
-                    //      git tag new_tag
-                    //      git push origin --tags
-                    //      NEW_TAG = new_tag
-                    //      docker tag image:BASE_TAG image:NEW_TAG
-                    //      docker push
-                    // if branch == 'staging':
-                    //      NEW_TAG = sh("git tag") + "_staging"
-                    //      NEW_TAG2 = sh("git tag") + "_staging" + BUILD_NUMBER
-                    //      docker tag image:BASE_TAG image:NEW_TAG
-                    //      docker tag image:BASE_TAG image:NEW_TAG2
-                    //      docker push
-
+            stage("Determine Version") {
+                steps {
+                    script {
+                        def baseVersion
+                        if (env.GIT_REPOSITORY_BRANCH == "master") {
+                            def currentTag = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
+                            baseVersion = bump_version(currentTag)
+                            echo "About to bump git tag to ${env.NEW_TAG} and push to repository"
+                            sh """
+                                git tag -a ${baseVersion} -m "Release ${baseVersion}" HEAD
+                                git push origin ${baseVersion}
+                            """
+                        } else {
+                            baseVersion = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
+                        }
+                        env.PROMO_TAGS = get_promotion_tags(baseVersion).join(" ")
+                        env.TAG = get_base_tag()
+                    }
+                }
+            }
+            stage("Docker Build base tag") {
+                steps {
+                    script {
+                        String buildCommand = params.DOCKER_USE_CACHE ? 'docker-compose build' : 'docker-compose build --no-cache'
+                        String pushCommand = params.DOCKER_RUN_PUSH   ? 'docker-compose push'  : 'echo "DOCKER_RUN_PUSH is set to false, skipping service "'
+                        sh """
+                            source /etc/profile >/dev/null 2>&1
+                            docker-compose pull --ignore-pull-failures -q --include-deps
+                            for p in \$(docker-compose config --services); do
+                                echo ">>> running ${buildCommand} for \$p"
+                                if ! ${buildCommand} "\$p"; then
+                                    echo "ERROR Failed to build \$p"
+                                    exit 1
+                                fi
+                                ${pushCommand} "\$p"
+                            done
+                        """
+                    }
+                }
+            }
+            stage("Retag & Push") {
+                steps {
+                    script {
+                        sh """
+                            for img in \$(docker-compose config --images); do
+                                IMAGE_NAME=\$(echo "\$img" | cut -d':' -f1)
+                                for t in ${env.PROMO_TAGS}; do
+                                    docker tag "\$img" "\${IMAGE_NAME}:\$t"
+                                    docker push "\${IMAGE_NAME}:\$t"
+                                done
+                            done
+                        """
+                    }
+                }
+            }
+            stage("Cleanup") {
+                steps {
+                    script {
+                        // todo: this also must delete promoted tags
+                        sh """
+                            for p in \$(docker-compose config | grep 'image:' | awk -F \\: '{ print \$2 }'); do
+                                i=\$(docker images -q \$p)
+                                [[ -n \$i ]] && docker rmi \$i --force
+                            done
+                        """
+                    }
                 }
             }
         }
+    }
+}
+
+def bump_version(version) {
+    def parts = version.tokenize(".")
+
+    if (parts.size() != 3) {
+        error("Version format must be major.minor.patch")
+    }
+
+    def major = parts[0].toInteger()
+    def minor = parts[1].toInteger()
+    def patch = parts[2].toInteger()
+    patch++
+    def newVersion = "${major}.${minor}.${patch}"
+    return newVersion
+}
+
+def get_promotion_tags(version){
+    if (env.GIT_REPOSITORY_BRANCH == "master"){
+        return [version]
+    }
+    else if (env.GIT_REPOSITORY_BRANCH == "staging"){
+        return ["${version}-staging", "${version}-staging.${env.BUILD_NUMBER}"]
+    } else {
+        return []
+    }
+
+}
+
+def get_base_tag(){
+    if (env.GIT_REPOSITORY_BRANCH == "master"){
+        return "master"
+    }
+    else if (env.GIT_REPOSITORY_BRANCH == "staging"){
+        return "staging"
+    } else {
+        // ? is there are anything more reliable?
+        return sh(script: "git describe --tags", returnStdout: true).trim()
     }
 }
