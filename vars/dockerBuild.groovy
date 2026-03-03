22f96ebfd14e890114646f787372e9d69283cb7e
bl1nkker <nurovich14@gmail.com>
2026-03-03 17:32:51 +0500

    added staging -> master promotion

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -44,78 +44,159 @@ def call(String useDockerfile = ''){
                     }
                 }
             }
-            stage("Determine Version") {
-                steps {
-                    script {
-                        def baseVersion
-                        if (env.GIT_REPOSITORY_BRANCH == "master") {
-                            def currentTag = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
-                            baseVersion = bump_version(currentTag)
-                            echo "About to bump git tag to ${env.NEW_TAG} and push to repository"
-                            sh """
-                                git tag -a ${baseVersion} -m "Release ${baseVersion}" HEAD
-                                git push origin ${baseVersion}
-                            """
-                        } else {
-                            baseVersion = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
-                        }
-                        env.PROMO_TAGS = get_promotion_tags(baseVersion).join(" ")
-                        env.TAG = get_base_tag()
-                        echo "Determined BASE_TAG: ${env.TAG}"
-                        echo "Determined PROMO_TAGS: ${env.PROMO_TAGS}"
-                    }
+            stage("Resolve Release Version for non-master branch") {
+              when {
+                not {
+                  environment name: 'GIT_REPOSITORY_BRANCH', value: 'master'
                 }
+              }
+              steps {
+                script {
+                  // The current git tag is selected as the current version (ex. "1.0.1")
+                  def currentVersion = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
+                  env.TAG = resolveBaseTag()
+                  echo "Got base tag: ${env.TAG}"
+                  env.PROMO_TAGS = generatePromotionTags(currentVersion).join(" ")
+                  echo "Got promotion tags: ${env.PROMO_TAGS}"
+                }
+              }
             }
-            stage("Docker Build") {
-                steps {
-                    script {
-                        Boolean result
-                        if (useDockerfile){
-                            Map res = runDockerfileBuild(useDockerfile)
-                            (dockerfileBuildImageId, dockerfileBuildContainerId) =
-                                res.isSuccessful ? [res.layerId, ''] : ['', res.layerId]
-                            echo "Got isSuccessful=${res.isSuccessful}, dockerfileBuildImageId=${dockerfileBuildImageId}, dockerfileBuildContainerId=${dockerfileBuildContainerId}"
+            stage("Resolve Release Version for master branch") {
+              when {
+                environment name: 'GIT_REPOSITORY_BRANCH', value: 'master'
+              }
+              
+              steps {
+                script {
+                  // If the build is started for the master branch, a new tag will be created and pushed to the git repository
+                  def currentVersion = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
+                  def newVersion = incrementPatchVersion(currentVersion)
+                  echo "About to bump git tag from ${currentVersion} to ${newVersion} and push to repository"
+                  sh """
+                      git tag -a ${newVersion} -m "Release ${newVersion}" HEAD
+                      git push origin ${newVersion}
+                  """
+                  def currentVersion = sh(script: "git tag --sort=-v:refname | head -n 1", returnStdout: true).trim()
+                  // TODO: add comment here why we use this for master branch
+                  env.TAG = currentVersion + "-staging"
+                  echo "Got base tag: ${env.TAG}"
+                  env.PROMO_TAGS = generatePromotionTags(currentVersion).join(" ")
+                  echo "Got promotion tags: ${env.PROMO_TAGS}"
+                }
+              }
+            }
+            stage('Build Docker Images for non-master branch'){
+              // This stage should always run IF build skipping is NOT enabled (default)
+              // Build skipping is useful when there is a need to "build" all jobs
+              // successfully without running actual build process (e.g. when creating
+              // hundreds of job items simultaneously via DSL)
+              when {
+                allOf {
+                  expression { !skipBuildCompletely }
+                  not {
+                    environment name: 'GIT_REPOSITORY_BRANCH', value: 'master'
+                  }
+                }
+              }
+
+              // Set environment for this stage: removed 
+              // since 'env.getEnviromnet()' will be without these values
+              // this is incompatible with later steps
+
+              steps {
+                echo "Effective value of TAG environment variable is '${env.TAG}' (Groovy)"
+                // Extra assertion to ensure that TAG variable is not set as literal "null"
+                // Possible cause: https://issues.jenkins.io/browse/JENKINS-43632
+                script {
+                  if (env.GIT_REPOSITORY_BRANCH != 'null') {
+                    assert sh(script: 'echo $TAG', returnStdout: true).toString().trim() != 'null'
+                  }
+                }
+
+                script {
+                  // Run Docker Build or Docker Compose Build depending on useDockerfile parameter value
+                  Map result
+                  if (useDockerfile){
+                    result = runDockerfileBuild(useDockerfile)
+                    (dockerfileBuildImageId, dockerfileBuildContainerId) = result.isSuccessful ? [result.layerId, ''] : ['', result.layerId]
+                  } else {
+                    result = runDockerComposeBuild()
+                  }
+                  if (!result.isSuccessful){
+                    error('Docker build failure')
+                  }
+                }
+              }
+            }
+            stage('Pull Images for master branch'){
+              // Must pull images with "<version>-staging" tag
+              when {
+                environment name: 'GIT_REPOSITORY_BRANCH', value: 'master'
+              }
+
+              steps {
+                if (useDockerfile){
+                  def prevImageTag = infraImageTagFromGitRepo(env.TAG)
+                  echo "Pulling Dockerfile image: ${prevImageTag}"
+                  def status = sh(script: "docker pull ${prevImageTag}", returnStatus: true)
+                  if (status != 0) {
+                      error("Failed to pull Dockerfile image: ${prevImageTag}")
+                  }
+                  dockerfileBuildImageId = sh(script: "docker images -q ${prevImageTag}", returnStdout: true).trim()
+                  if (!dockerfileBuildImageId) {
+                      error("Dockerfile image ID not found for ${prevImageTag}")
+                  }
+                  echo "Dockerfile image ID found: ${dockerfileBuildImageId}"
+                } else {
+                    echo "Pulling Docker Compose images..."
+                    def status = sh(script: "docker-compose pull", returnStatus: true)
+                    if (status != 0) {
+                        error("Failed to pull Docker Compose images")
+                    }
+                    def images = sh(script: "docker-compose config --images", returnStdout: true).trim().split("\n")
+                    for (img in images) {
+                        def exists = sh(script: "docker images -q ${img}", returnStdout: true).trim()
+                        if (!exists) {
+                            error("Image ${img} is missing after pull")
                         } else {
-                            result = runDockerComposeBuild()
-                            if (!result){
-                                error('Docker build failure')
-                            }
+                            echo "Verified image: ${img}"
                         }
                     }
                 }
+              }
             }
             stage("Promote & Publish Images") {
-                steps {
-                    script {
-                        List images = []
-                        def tags = [env.TAG] + PROMO_TAGS.tokenize(" ")
-                        if (useDockerfile){
-                            for (tag in tags) {
-                                def imageTag = infraImageTagFromGitRepo(tag)
-                                images = images + promoteDockerfileImage(dockerfileBuildImageId, imageTag)
-                            }
-                            } else{
-                            for (tag in tags) {
-                                images = images + promoteDockerComposeImages(tag)
-                            }
-                        }
-                        echo "images: $images"
-                        if (params.DOCKER_RUN_PUSH){
-                            echo "DOCKER_RUN_PUSH is set to true, publishing images"
-                            for (img in images){
-                                publishDockerImage(img)
-                            }
-                        } else{
-                            echo "DOCKER_RUN_PUSH is set to false, skipping service"
-                        }
+              steps {
+                script {
+                  List images = []
+                  def tags = [env.TAG] + PROMO_TAGS.tokenize(" ")
+                  if (useDockerfile){
+                    for (tag in tags) {
+                      def image = promoteDockerfileImage(dockerfileBuildImageId, infraImageTagFromGitRepo(tag))
+                      images.add(image)
                     }
+                  } else{
+                    for (tag in tags) {
+                      images = images + promoteDockerComposeImages(tag)
+                    }
+                  }
+                  echo "images: $images"
+                  if (params.DOCKER_RUN_PUSH){
+                    echo "DOCKER_RUN_PUSH is set to true, publishing images"
+                    for (img in images){
+                      publishDockerImage(img)
+                    }
+                  } else{
+                    echo "DOCKER_RUN_PUSH is set to false, skipping service"
+                  }
                 }
+              }
             }
         }
     }
 }
 
-def bump_version(version) {
+def incrementPatchVersion(version) {
     def parts = version.tokenize(".")
 
     if (parts.size() != 3) {
@@ -130,7 +211,7 @@ def bump_version(version) {
     return newVersion
 }
 
-def get_promotion_tags(version){
+def generatePromotionTags(version){
     if (env.GIT_REPOSITORY_BRANCH == "master"){
         return [version]
     }
@@ -142,7 +223,7 @@ def get_promotion_tags(version){
 
 }
 
-def get_base_tag(){
+def resolveBaseTag(){
     if (env.GIT_REPOSITORY_BRANCH == "master"){
         return "master"
     }
