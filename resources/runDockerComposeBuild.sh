f51969a5e32684cf9c0d61611829fa892413abec
bl1nkker <nurovich14@gmail.com>
2026-03-05 17:59:57 +0500

    fixed args order during docker compose build

--- resources/runDockerComposeBuild.sh
+++ resources/runDockerComposeBuild.sh
@@ -6,7 +6,7 @@ source /etc/profile >/dev/null 2>&1
 docker-compose pull --ignore-pull-failures -q --include-deps
 for p in $(docker-compose config --services); do
     echo ">>> Building $p"
-    docker-compose build "$p" ${CACHE_ARGS}
+    docker-compose build ${CACHE_ARGS} "$p"
 done
 
 exit 0
