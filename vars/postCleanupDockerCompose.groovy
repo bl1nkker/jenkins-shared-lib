fbedfc00427bcfc8fa7168a1d3245e6ed1d411dd
bl1nkker <nurovich14@gmail.com>
2026-03-06 17:23:38 +0500

    fixed image_id definition

--- vars/postCleanupDockerCompose.groovy
+++ vars/postCleanupDockerCompose.groovy
@@ -1,8 +1,9 @@
 def call() {
     def scriptContent = """
         for img in \$(docker-compose config | awk '/image:/ {print \$2}'); do
-            IMAGE_ID=\$(docker images -q "\$img")
-            echo "Cleaning up image=\$img with image_id=\$IMAGE_ID ..."
+            IMAGE_NAME=\$(echo "\$img" | cut -d':' -f1)
+            IMAGE_ID=\$(docker images -q "\$IMAGE_NAME")
+            echo "Cleaning up image=\$IMAGE_NAME with image_id=\$IMAGE_ID ..."
             docker image rm "\$IMAGE_ID" --force
         done
     """
